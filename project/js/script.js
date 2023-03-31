e3bf69b7be5419e835f6294e8cefd8fd5a62d63c
Malika-Mirxojiyeva <105709704+Malika-Mirxojiyeva@users.noreply.github.com>
2023-03-31 20:05:10 +0500

    Design createdd

---
+++ project/js/script.js
@@ -0,0 +1,5 @@
+function randomWord() {
+    let ranObj = wordList[Math.floor(Math.random() *wordList.length)];
+    console.log(ranObj);
+}
+randomWord();
---
+++ project/js/word.js
@@ -0,0 +1,14 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "afraid",
+        hint: "qo'rqmoq"
+    },
+    {
+        word: "name",
+        hint: "ism"
+    }
+];
