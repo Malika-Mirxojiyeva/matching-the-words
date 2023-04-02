5efc8b2f1366346a00edfabc77752ea14d75d8d5
Malika-Mirxojiyeva <105709704+Malika-Mirxojiyeva@users.noreply.github.com>
2023-04-02 16:01:14 +0500

    Js code update

--- project/js/script.js
+++ project/js/script.js
@@ -1,44 +1,84 @@
 const wordList = [
     {
-        word: "run",
-        hint: "yugurmoq"
+      word: "run",
+      hint: "yugurmoq"
     },
     {
-        word: "afraid",
-        hint: "qo'rqmoq"
+      word: "afraid",
+      hint: "qo'rqmoq"
     },
     {
-        word: "name",
-        hint: "ism"
+      word: "name",
+      hint: "ism"
     }
-];
-
-const inputs = document.querySelector(".inputs");
-const resetBtn = document.querySelector(".reset-btn");
-const hint = document.querySelector(".hint span");
-const typingInput = document.querySelector(".typing-input");
-function randomWord() {
+  ];
+  
+  const inputs = document.querySelector(".inputs");
+  const resetBtn = document.querySelector(".reset-btn");
+  const hint = document.querySelector(".hint span");
+  const guessLeft = document.querySelector(".guess-left span");
+  const wrongLetter = document.querySelector(".wrong-letter span");
+  
+  let word = "";
+  let wrongLetters = [];
+  let remainingGuesses = 10;
+  
+  function randomWord() {
     //getting random object from wordList
-    var ranObj = wordList[Math.floor(Math.random() *wordList.length)];
-    var word = ranObj.word;
-    console.log(word);
-
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranObj.word;
+  
     hint.innerText = ranObj.hint;
-
-    var html = "";
-    for (let i = 0; i < word.length; i++){
-        html += '<input type="text" disabled maxlength="1">';
+  
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+      html += '<input type="text" class="typing-input" maxlength="1" id="typing-input"> ';
     }
-
-    document.querySelector('.inputs').innerHTML = html;
-}
-
-randomWord();
-
-function initgame(e) {
-    let 
-}
-
-resetBtn.addEventListener("click", randomWord);
-typingInput.addEventListener("input", initGame);
-document.addEventListener("keydown", () => typingInput.focus());
+  
+    inputs.innerHTML = html;
+    wrongLetters = [];
+    remainingGuesses = 10;
+    guessLeft.innerText = remainingGuesses;
+    wrongLetter.innerText = "";
+  }
+  
+  function initGame(e) {
+    let key = e.target.value;
+    if (key) {
+      if (word.includes(key)) {
+        let inputsArray = document.querySelectorAll(".typing-input");
+        inputsArray.forEach((input, index) => {
+          if (word[index] === key) {
+            input.value = key;
+          }
+        });
+  
+        if (!inputs.innerHTML.includes('class="typing-input"')) {
+          console.log("You won!");
+          guessLeft.innerText = "0";
+        }
+      } else {
+        if (wrongLetters.includes(key)) {
+          return;
+        }
+        wrongLetters.push(key);
+        wrongLetter.innerText = wrongLetters.join(", ");
+        remainingGuesses--;
+        guessLeft.innerText = remainingGuesses;
+        if (remainingGuesses === 0) {
+          console.log("You lost!");
+          guessLeft.innerText = "0";
+        }
+      }
+    }
+  }
+  
+  function resetGame() {
+    randomWord();
+  }
+  
+  inputs.addEventListener("keyup", initGame);
+  resetBtn.addEventListener("click", resetGame);
+  
+  randomWord();
+  
